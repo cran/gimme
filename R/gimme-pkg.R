52e598b4e2b1653bca4c15460bac51a7088e3865
Stephanie Lane <slane@unc.edu>
2017-10-27 09:44:18 +0000

    version 0.3-1

--- R/gimme-pkg.R
+++ R/gimme-pkg.R
@@ -1,17 +1,19 @@
 #' @name gimme-package
 #' @aliases gimme-package
 #' @title Group iterative multiple model estimation
-#' @description This package contains functions to identify group- and individual-level structural equation models.
+#' @description This package contains functions to identify group- and individual-level unified structural equation models.
 #' @author {Stephanie Lane [aut, cre, trl],\cr 
 #'    Kathleen Gates [aut], \cr
 #'    Peter Molenaar [aut], \cr
 #'    Michael Hallquist [ctb], \cr
-#'    Hallie Pike [ctb]  
+#'    Hallie Pike [ctb],  \cr
+#'    Zachary Fisher [ctb],  \cr
+#'    Cara Arizmendi [ctb]
 #' }
 #' Maintainer: Stephanie Lane \email{slane@@unc.edu}
-#' @import lavaan gWidgets
+#' @import lavaan 
 #' @importFrom grDevices dev.off pdf
-#' @importFrom stats aggregate ave complete.cases qchisq qnorm reshape time cov sd
+#' @importFrom stats aggregate ave complete.cases qchisq qnorm reshape time cov sd na.omit
 #' @importFrom utils capture.output head read.table write.csv write.table file_test read.csv
 #' @importFrom qgraph qgraph
 #' @importFrom igraph graph.adjacency walktrap.community modularity
